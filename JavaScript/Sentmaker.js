8a1354b69a0ee2a6af43d732056653e17c360e79
Diyajabeen <118273800+Diyajabeen@users.noreply.github.com>
2025-12-13 20:15:35 +0530

    sentence maker

---
+++ JavaScript/Sentmaker.js
@@ -0,0 +1,17 @@
+let adjective = "beautiful";
+let noun = "girl";
+let verb = "angry";
+let place = "Village";
+let adjective2 = "fiery";
+let noun2 = "cake";
+let firstStory ="Once upon a time, there was a(n) "+adjective+" "+noun + " who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+ " nostrils that blew fire when it was "+verb+".";
+console.log("First story: " + firstStory);
+"First story: [firstStory]"
+adjective = "kind";
+noun = "boy";
+noun2 = "pie";
+place = "forest";
+adjective2 = "fairy";
+verb = "sad";
+let secondStory ="Once upon a time, there was a(n) "+adjective+" "+noun + " who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+ " nostrils that blew fire when it was "+verb+".";
+console.log("Second story: " + secondStory);
